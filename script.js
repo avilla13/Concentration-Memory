7ae09189c195a11d251c71a34b2830861445460b
avilla13 <xetox13@gmail.com>
2023-06-20 23:56:34 -0500

    Update the card class structure for easier refactoring

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 /*----CONSTANTS----*/
-const cardDeck = [
-
-
-];
+const cardDeck = [];
 
 const cardTypes = [
     {id: 0, img: 'blue'},
@@ -14,31 +11,31 @@ const cardTypes = [
     {id: 6, img: 'yellow'},
 ];
 
-
-
 /*----game STATE variables----*/
 let isFirstClick = true; // Variable to track the first click
-let gameboard; // Variable to track state of cards
+ // Variable to track state of cards
 let playerStats = [ // Variable to track player choices and stats
-    {choice1: 'null', choice2: 'null'},
+    {choices: []},
     {clicks: 0, pairs: 0},
     {currCard: 'null'}
 ]; 
+let cardChoices = []; // Empty array to hold player choices
 let timer;
-    
 
 
 /*----CACHED elements----*/
 
 const cardEls = document.querySelectorAll('.card'); // Get all the elements with the class 'card'
+const gameboard = document.getElementById('gameboard'); // Get all the elements in gameboard
 
 
 /*----EVENT listeners----*/
 
 // Add a click event listener to each card element -> triggers the handleClick()
-cardEls.forEach((cardEl) => {
-  cardEl.addEventListener('click', handleClick);
-});
+// cardEls.forEach((cardEl) => {
+//   cardEl.addEventListener('click', handleClick);
+// });
+gameboard.addEventListener('click', handleClick);
 
 
 /*----FUNCTIONS----*/
@@ -68,7 +65,6 @@ function renderBoard() {
     // Assign the 'images' from the cardTypes to each card element
         cardVisibleEls.forEach((cardVisibleEl, index) => {
         cardVisibleEl.style.backgroundColor = cardTypes[index].img;
-        // cardVisibleEl.style.visibility = 'hidden';
     });
 
   }
@@ -83,25 +79,38 @@ function handleClick(evt) {
     // Set isFirstClick to false, so it won't execute this block on subsequent clicks
     isFirstClick = false;
   }
+  // Store first card and second card in the 'cardChoices' array, respectively
+  const cardChoice = evt.target.style.backgroundColor;
+  for(let i =0; i < 2; i++){
+    cardChoices[i] = cardChoices.push(cardChoice)};
 
-  const cardVisible = this.querySelector('.card-visible'); // access 'card-visible' class
-  const cardHidden = this.querySelector('.card-hidden'); // access 'card-hidden' class
+    console.log(cardChoices);
 
-  this.classList.toggle('card-visible'); // toggles the clicked card to class 'card-visible'
-  this.classList.toggle('card-hidden'); // toggles the clicked card to class 'card-hidden'
+
+//   const cardVisible = document.querySelector('.card-visible'); // access 'card-visible' class
+//   const cardHidden = document.querySelector('.card-hidden'); // access 'card-hidden' class
+
+//   cardVisible.classList.toggle('card-visible'); // toggles the clicked card to class 'card-visible'
+//   this.classList.toggle('card-hidden'); // toggles the clicked card to class 'card-hidden'
   
 
-  if (cardVisible.style.display === 'none') {
-    cardVisible.style.visibility === 'visible';
-    cardVisible.style.display = 'block';
-    cardHidden.style.display = 'none';
-  } else {
-    cardVisible.style.display = 'none';
-    cardHidden.style.display = 'block';
-  }
+//   if (cardVisible.style.display === 'none') {
+    
+//     cardVisible.style.display = 'block';
+//     cardHidden.style.display = 'none';
+//   } else {
+//     cardVisible.style.display = 'none';
+//     cardHidden.style.display = 'block';
+//   }
 }
   
-
+function matchPairs (card1, card2) { // check if selected cards are a match
+    if (card1 === card2) { // if they are, add 1 to scores
+        scores++;
+    } else{
+        return; // if they are not, then clear playerStats.choices
+    }
+}
 
 function renderTimer() {
   return console.log(`First card has been selected, the timer has begun!`);

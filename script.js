034c777426030caf93488f2bf081b4a64c5530a4
avilla13 <xetox13@gmail.com>
2023-06-21 21:35:09 -0500

    Debug matchPairs function for card selection

--- script.js
+++ script.js
@@ -3,17 +3,16 @@
 const cardDeck = [];
 
 const cardTypes = [
-    {id: 0, img: 'blue'},
-    {id: 1, img: 'blue'},
-    {id: 2, img: 'red'},
-    {id: 3, img: 'red'},
-    {id: 4, img: 'yellow'},
-    {id: 6, img: 'yellow'},
+    {id: 0, img: 'blue', match: 0}, // match: 0 -> card not matched
+    {id: 1, img: 'blue', match: 0}, // match: 1 -> card has been matched
+    {id: 2, img: 'red', match: 0},
+    {id: 3, img: 'red', match: 0},
+    {id: 4, img: 'yellow', match: 0},
+    {id: 5, img: 'yellow', match: 0},
 ];
 
 /*----game STATE variables----*/
 let isFirstClick = true; // Variable to track the first click
- // Variable to track state of cards
 let playerStats = { // Variable to track player choices and stats
     choice1: null,
     choice2: null,
@@ -21,7 +20,6 @@ let playerStats = { // Variable to track player choices and stats
 }; 
 let cardChoices = []; // Empty array to hold player choices
 let timer;
-
 
 /*----CACHED elements----*/
 
@@ -31,10 +29,7 @@ const gameboard = document.getElementById('gameboard'); // Get all the elements 
 
 /*----EVENT listeners----*/
 
-// Add a click event listener to each card element -> triggers the handleClick()
-// cardEls.forEach((cardEl) => {
-//   cardEl.addEventListener('click', handleClick);
-// });
+// Add a click event listener to each gameboard element -> triggers the handleClick()
 gameboard.addEventListener('click', handleClick);
 
 
@@ -42,7 +37,6 @@ gameboard.addEventListener('click', handleClick);
 
 // DON'T FORGET TO CALL init() to initialize all state, then call render()
 init();
-
 function init() {
   scores = {
    playerOne: 0
@@ -58,7 +52,7 @@ function render() {
     renderScores();
     renderResults();
 }
-// render board-> initiate board shuffled grid (only once), 
+// render board-> initiate shuffled board grid (only once), 
 // then render state of cards whenever invoked
 function renderBoard() {
     // Assign the 'images' from the cardTypes to each card element w/ 'card-visible' class
@@ -71,10 +65,10 @@ function renderBoard() {
     })
 }
 
-
 function handleClick(evt) {
-  // guards for only clicking on card
-if (evt.target.classList.contains('card')){
+  // guards for only clicking on card elements
+if (evt.target.classList.contains('card') && 
+    cardTypes[parseInt(evt.target.id)].match === 0) { //  & to not select matched cards
     // Check if it's the first click
   if (isFirstClick) {
     // Start timer function
@@ -82,13 +76,15 @@ if (evt.target.classList.contains('card')){
     // Set isFirstClick to false, so it won't execute this block on subsequent clicks
     isFirstClick = false;
   }
-  evt.target.classList.toggle('card-visible'); // toggles the clicked card to class 'card-visible'
   
   // Store first card and second card in the 'playerStats.choice1 & 2' (respectively) and track clicks
-  const currentId = evt.target.id;
-  const cardChoice = cardTypes[parseInt(currentId)].img; 
-    console.log(cardChoice);
+  const cardId = parseInt(evt.target.id); // target the selected card's id property
+  const cardChoice = cardTypes[cardId].img; //use that id to select cardTypes' image
 
+  if (cardTypes[cardId].match === 0) { // checks to see if card has been matched
+      evt.target.classList.toggle('card-visible'); // toggles the clicked card's class to 'card-visible'
+  }
+    
   if (playerStats.clicks === 0) {
     playerStats.choice1 = cardChoice;
     playerStats.clicks++;
@@ -104,7 +100,7 @@ if (evt.target.classList.contains('card')){
   } 
 
     console.log(playerStats);
-    render();
+    render(); // render the card(s) state
     }
 }
 
@@ -112,18 +108,27 @@ function flipCard() {
 
 }
 
-function matchPairs (card1, card2) { // check if selected cards are a match
-
+function matchPairs (card1, card2) { // checks if selected cards are a match
     if (card1 === card2) { // if they are, add 1 to scores
         scores.playerOne++;
         console.log(`scores: ${scores.playerOne}`);
-        // card1 and card2 -> stay as card-visible class
+        // card1 and card2 -> stay as card-visible class and unselectable
+       // Find the indices of card1 and card2 in the cardTypes array
+        const card1Index = cardTypes.findIndex(card => card.img === card1);
+        const card2Index = cardTypes.findIndex(card => card.img === card2 && card.id !== card1Index);
+    console.log(card1Index, card2Index);
+    // Update the 'match' value to 1 for both cards
+        if (card1Index !== -1 && card2Index !== -1) {
+        cardTypes[card1Index].match = 1;
+        cardTypes[card2Index].match = 1;
+        }
+       
         // Add audio for matching pair
-        return card1, card2 = null;
+       
     } else { // if they are not, then clear playerStats.choices
         console.log(`card one ${card1} does not pair with card two ${card2}`);
         // Add audio for no-match
-        return card1, card2 = null;
+        
     }
 }
 
